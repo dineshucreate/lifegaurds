906f83491aa32269b722f88de1cca2faeab9c93e
Dinesh Rajput <dineshrajput@ucreate.co.in>
2019-06-13 17:03:51 +0530

    Added firebase and POD
     - Added googleServices for IOS  - changed pod config. -added configuration

--- App.js
+++ App.js
@@ -2,17 +2,52 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import codePush from 'react-native-code-push';
+import firebase from 'react-native-firebase';
+
 export default class App extends Component {
+
   componentDidMount = () => {
     codePush.sync({
       updateDialog: true,
       installMode: codePush.InstallMode.IMMEDIATE
     });
+    this.pushNotificationConfigure();
+  }
+
+  pushNotificationConfigure = async () => {
+    await firebase.messaging().hasPermission()
+      .then((enabled) => {
+        if (enabled) {
+          firebase.messaging().getToken()
+            .then((fcmToken) => {
+              if (fcmToken) {
+                alert(fcmToken);
+              }else{
+                alert('error');
+              }
+            });
+        } else {
+          firebase.messaging().requestPermission()
+            .then(() => {
+              firebase.messaging().getToken()
+                .then((fcmToken) => {
+                  if (fcmToken) {
+                    alert(fcmToken);
+                  }else{
+                    alert('error');
+                  }
+                });
+            })
+            .catch((error) => {
+              alert(JSON.stringify(error));
+            });
+        }
+      });
   }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>We were Life Guard!</Text>
+        <Text style={styles.welcome}>We are Life Guard!</Text>
       </View>
     );
   }
--- ios/lifegaurds/AppDelegate.m
+++ ios/lifegaurds/AppDelegate.m
@@ -7,15 +7,18 @@
 
 #import "AppDelegate.h"
 #import <CodePush/CodePush.h>
-
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <UserNotifications/UserNotifications.h>
+#import <React/RCTPushNotificationManager.h>
 
+@import Firebase;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"lifegaurds"
@@ -31,6 +34,33 @@
   return YES;
 }
 
+// Required to register for notifications
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
+{
+  [RCTPushNotificationManager didRegisterUserNotificationSettings:notificationSettings];
+}
+// Required for the register event.
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  [RCTPushNotificationManager didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+// Required for the notification event. You must call the completion handler after handling the remote notification.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+  [RCTPushNotificationManager didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+// Required for the registrationError event.
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  [RCTPushNotificationManager didFailToRegisterForRemoteNotificationsWithError:error];
+}
+// Required for the localNotification event.
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+  [RCTPushNotificationManager didReceiveLocalNotification:notification];
+}
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
@@ -39,5 +69,6 @@
   return [CodePush bundleURL];
 #endif
 }
+
 
 @end
